3eee4959f9107310b0eb5f4b30abea54732bcb1c
danilbo <61252751+danilbo@users.noreply.github.com>
2021-10-19 14:40:05 +0300

    Initial commit

---
+++ src/main/java/com/example/springapp/controllers/Controller.java
@@ -0,0 +1,17 @@
+package com.example.springapp.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/base")
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = ("whoever you are...")) String name) {
+        return String.format("Hello, %s!", name);
+    }
+}
