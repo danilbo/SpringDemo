7e7d526529e0246434f3a306deb3ab455e39ebcc
danilbo <61252751+danilbo@users.noreply.github.com>
2021-10-20 09:59:34 +0300

    test

--- src/main/java/com/example/springapp/controllers/Controller.java
+++ src/main/java/com/example/springapp/controllers/Controller.java
@@ -1,18 +1,26 @@
 package com.example.springapp.controllers;
 
 
+import com.example.springapp.entitys.Book;
+import com.example.springapp.repos.BookRep;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/base")
 public class Controller {
+
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = ("whoever you are...")) String name) {
         return String.format("Hello, %s!", name);
     }
+
 }
-//asdaddadad
+
+
